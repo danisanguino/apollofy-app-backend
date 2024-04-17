69622660d82fbc52ab36edf425eb9190f36c8767
unknown <josemari94@gmail.com>
2024-04-17 14:00:38 +0200

    updated and deleted track controller

--- src/controllers/track.controllers.ts
+++ src/controllers/track.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../db/prismaClient";
+import { deleteImageCloudinary, uploadImageCloudinary } from "../utils/cloudinary";
 
 export const getAllTracks = async (req: Request, res: Response) => {
   try {
@@ -11,35 +12,46 @@ export const getAllTracks = async (req: Request, res: Response) => {
 };
 
 export const createTrack = async (req: Request, res: Response) => {
-  const { title, url, thumbnail, albumId, artistId } = req.body;
+  const { title, url, albumId, artistId } = req.body;
+  const thumbnail = req.files?.thumbnail
   const { userId } = req.params;
+
   if (!title || !url || !thumbnail) {
     return res
       .status(400)
       .send("The fields title, url, thumbnail are required");
   }
   try {
-    const newTrack = await prisma.track.create({
-      data: {
-        title,
-        url,
-        thumbnail,
-        albumId,
-        artistId,
-        userId,
-      },
-    });
-    return res.status(201).send({
-      msg: "New track has been created successfully",
-      data: newTrack,
-    });
+    if (Array.isArray(thumbnail)) {
+      return res.status(400).json({
+        msg: "You can only upload one file per user.",
+      });
+    } else {
+      const result = await uploadImageCloudinary(thumbnail.tempFilePath); // Subir el archivo único
+      const newTrack = await prisma.track.create({
+        data: {
+          title,
+          url,
+          albumId,
+          artistId,
+          userId,
+          thumbnail: result.secure_url,
+          public_id_thumbnail: result.public_id,
+        },
+      });
+      return res.status(201).send({
+        msg: "New track has been created successfully",
+        data: newTrack,
+      });
+    }
   } catch (error) {
     return res.status(400).send(error);
   }
 };
 
 export const updateTrack = async (req: Request, res: Response) => {
-  const { title, url, thumbnail, albumId, artistId } = req.body;
+  const { title, url, albumId, artistId } = req.body;
+  const thumbnail = req.files?.thumbnail
   const { userId } = req.params;
   try {
     const updatedTrack = await prisma.track.update({
@@ -47,16 +59,39 @@ export const updateTrack = async (req: Request, res: Response) => {
       data: {
         title,
         url,
-        thumbnail,
         albumId,
         artistId,
         userId,
       },
     });
-    res
+    if (req.files && thumbnail) {
+      //Eliminar la imagen para volverla a subir
+      //Elimina
+      if(updatedTrack.public_id_thumbnail) {
+        await deleteImageCloudinary(updatedTrack.public_id_thumbnail)
+      }
+      //Sube mismo metodo que el create
+      if (Array.isArray(thumbnail)) {
+        return res.status(400).json({
+          msg: "You can only upload one file per user.",
+        });
+      } else {
+        const result = await uploadImageCloudinary(thumbnail.tempFilePath); // Subir el archivo único
+        const newTrackThumbnail = await prisma.track.update({
+          where: { id: userId },
+          data: {
+            thumbnail: result.secure_url,
+            public_id_thumbnail: result.public_id,
+          },
+        });
+        return res
+      .status(201)
+      .send({ msg: "The track has been updated", data: newTrackThumbnail });
+      } }
+      return res
       .status(201)
       .send({ msg: "The track has been updated", data: updatedTrack });
-  } catch (error) {
+     } catch (error) {
     res.status(400).send({ msg: "ERROR" });
   }
 };
@@ -66,6 +101,7 @@ export const deleteTrack = async (req: Request, res: Response) => {
 
   try {
     const trackDeleted = await prisma.track.delete({ where: { id: trackId } });
+    await deleteImageCloudinary(trackDeleted.public_id_thumbnail)
     res
       .status(201)
       .send({ msg: "Track has been deleted successfully", data: trackDeleted });
