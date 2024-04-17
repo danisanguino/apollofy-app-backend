b2cd22f3bfce3e26ff513af66319c07459672c04
Julia Sanchis <jursanchis@gmail.com>
2024-04-17 08:56:09 +0200

    implementing user controllers

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,57 +1,78 @@
 import { Request, Response } from "express";
-import prisma from "../db/prismaClient"
+import prisma from "../db/prismaClient";
 
 export const getAllUsers = async (req: Request, res: Response) => {
-    
-
-    try {
-
-        console.log("Ji ome")
-        res.status(201).send("Te traigo todo los users calentitos")
-    } catch (error) {
-        
-    }
-}
+  try {
+    const allUsers = await prisma.user.findMany();
+    res.status(201).send({ msg: "Here are all your users", data: allUsers });
+  } catch (error) {
+    res.status(400).send({ msg: "Error", error });
+  }
+};
 
 export const createUser = async (req: Request, res: Response) => {
-    const {name, lastname, email, password, img, username, role} = req.body
-    console.log("hola")
-    if(!name || !lastname || !email || !password || !username) {
-        return res.status(400).send("Te falta calle")
-    }
-    try {
-        const newUser = await prisma.user.create({
-            data: {
-                name,
-                lastname,
-                email,
-                password,
-                username,
-            }
-        })
-        console.log("Ji ome un usuario nuevo")
-        return res.status(201).send({msg: "He añadido un usuario nuevo calentito tambien", data: newUser})
-    } catch (error) {
-        return res.status(400).send(error)
-    }
-}
+  const { name, lastname, email, password, img, username, role } = req.body;
+  if (!name || !lastname || !email || !password || !username) {
+    return res
+      .status(400)
+      .send(
+        "The fields name, lastname, email, password and username are required"
+      );
+  }
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        lastname,
+        email,
+        password,
+        img,
+        username,
+        role,
+      },
+    });
+    return res.status(201).send({
+      msg: "New user created",
+      data: newUser,
+    });
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
 
 export const updateUser = async (req: Request, res: Response) => {
-    
-    try {
-        console.log("Ji ome te he modificado un usuario")
-        res.status(201).send("He actualizado un usuario calentito tambien")
-    } catch (error) {
-        
-    }
-}
+  const { name, lastname, email, password, img, username, role } = req.body;
+  const { userId } = req.params;
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        name,
+        lastname,
+        email,
+        password,
+        img,
+        username,
+        role,
+      },
+    });
+    res
+      .status(201)
+      .send({ msg: "The user has been updated", data: updatedUser });
+  } catch (error) {
+    res.status(400).send({ msg: "ERROR" });
+  }
+};
 
 export const deleteUser = async (req: Request, res: Response) => {
-    
-    try {
-        console.log("Ji ome te he borrao un usuario")
-        res.status(201).send("He borrao un usuario calentito tambien")
-    } catch (error) {
-        
-    }
-}
+  const { userId } = req.params;
+
+  try {
+    const userDeleted = await prisma.user.delete({ where: { id: userId } });
+    res
+      .status(201)
+      .send({ msg: "User has been deleted successfully", data: userDeleted });
+  } catch (error) {
+    res.status(400).send({ msg: "ERROR" });
+  }
+};
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,16 @@
-import { Router }  from "express";
-import { createUser, deleteUser, getAllUsers, updateUser } from "../controllers/users.controllers";
+import { Router } from "express";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  updateUser,
+} from "../controllers/users.controllers";
 
 const userRoutes = Router();
 
-userRoutes.get("/", getAllUsers)
-userRoutes.post("/", createUser)
-userRoutes.patch("/", updateUser)
-userRoutes.delete("/", deleteUser)
+userRoutes.get("/", getAllUsers);
+userRoutes.post("/", createUser);
+userRoutes.patch("/:userId", updateUser);
+userRoutes.delete("/:userId", deleteUser);
 
 export default userRoutes;
