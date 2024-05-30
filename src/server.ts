c9fdc95b9c5e238622c8bf714fd5c42dcda97f3b
Maria Cidoncha <mariacidoncha@gmail.com>
2024-05-30 12:48:30 +0200

    feat: add welcome msg to /

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import userRoutes from './routes/user.routes';
 import trackRoutes from './routes/track.routes';
 import { urlencoded } from 'body-parser';
@@ -25,5 +25,11 @@ app.use('/genre', checkJwtMiddleware, genreRoutes);
 app.use('/playlist', checkJwtMiddleware, playlistRoutes);
 app.use('/artist', checkJwtMiddleware, artistsRoutes);
 app.use('/album', checkJwtMiddleware, albumsRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: 'Welcome to Apollofy API.',
+  });
+});
 
 export default app;
