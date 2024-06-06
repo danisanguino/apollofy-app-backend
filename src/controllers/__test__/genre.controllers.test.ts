d7ba5e4925e2238ac2aa88ba4f313483db0bf144
Julia Sanchis <jursanchis@gmail.com>
2024-06-06 09:37:23 +0200

    add test inside genres controller

--- src/controllers/__test__/genre.controllers.test.ts
+++ src/controllers/__test__/genre.controllers.test.ts
@@ -1,1 +1,17 @@
 const { getAllGenres } = require("../genre.controllers");
+import { describe, expect, test } from "@jest/globals";
+
+describe("get all genres", () => {
+  test("testing 1+1", () => {
+    expect(1 + 1).toBe(2);
+  });
+  // test("tiene que devolver COSAS", () => {
+  //   const mockReq = {};
+  //   const mockRes = {
+  //     status: jest.fn(),
+  //     send: jest.fn(),
+  //   };
+  //   getAllGenres(mockReq, mockRes);
+  //   expect(mockRes.status).toHaveBeenCalled();
+  // });
+});
